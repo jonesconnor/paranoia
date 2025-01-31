7a03528e32304996fc22aba5b909069f1923f5d4
Connor Jones <connorj@hey.com>
2025-01-30 21:29:00 -0800

    PN-001: Establish database connectivity
    Incorporate SQLAlchemy into the application for POST and GET calls.
    Additionally, create two new endpoints for generating a uuid upon a
    secret being submitted and for retrieving a secret using the uuid.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from .database import get_db
+from .models import Secret
+
+from uuid import uuid4
 
 app = FastAPI()
 
@@ -8,6 +13,9 @@ data_store = {}
 
 class DataModel(BaseModel):
     data: str
+
+class SecretModel(BaseModel):
+    secret: str
 
 @app.get("/")
 def hello_world():
@@ -31,3 +39,25 @@ async def delete_data(key: int):
         del data_store[key]
         return JSONResponse(status_code=200, content={"message": "Data deleted successfully!"})
     return JSONResponse(status_code=404, content={"message": "Data not found! Lost in the cloud!"})
+
+@app.post("/generateuuid")
+async def generate_uuid(secret: SecretModel, db: Session = Depends(get_db)):
+    uuid = str(uuid4())
+    secret = Secret(secret=secret.secret, uuid=uuid)
+    db.add(secret)
+    db.commit()
+    db.refresh(secret)
+    return JSONResponse(status_code=201, content={"message": "Secret stored successfully!",
+                                                  "uuid": uuid,
+                                                  "url": f"http://127.0.0.1:8000/getsecret/{uuid}"})
+
+@app.get("/getsecret/{uuid}")
+async def get_secret(uuid: str, db: Session = Depends(get_db)):
+    secret = db.query(Secret).filter(Secret.uuid == uuid, Secret.remaining_accesses > 0).first()
+    if secret:
+        secret.remaining_accesses -= 1
+        if secret.remaining_accesses == 0:
+            db.delete(secret)
+        db.commit()
+        return {"secret": secret.secret}
+    return JSONResponse(status_code=404, content={"message": "Secret not found!"})
---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from .database import Base
+from datetime import datetime, timezone
+
+class Secret(Base):
+    __tablename__ = 'secrets'
+
+    id = Column(Integer, primary_key=True, index=True)
+    secret = Column(Text, nullable=False)
+    uuid = Column(String, unique=True, index=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    remaining_accesses = Column(Integer, default=1)
